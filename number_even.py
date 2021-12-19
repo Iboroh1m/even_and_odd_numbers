f3b317f3056f41067dea50f7ead8f816ee41cf67
Iboroh1m <boymirzayev20031310@gmail.com>
2021-12-19 00:19:33 -0800

    sum of even numbers

--- number_even.py
+++ number_even.py
@@ -1,5 +1,12 @@
-#A four-digit integer is given. Find the number of even digits in it.
-
-#Create a variable "var_int" and assign it a four-digit integer value.
-
-#Print the number of even digits in the variable "var_int".
+var_int = 7824
+a = var_int // 10             
+b = var_int % 10              
+c = a %10                     
+a = a //10                     
+d = a % 10                   
+e = a // 10
+b = ((b % 2)+1)%2
+c = ((c % 2)+1)%2
+d = ((d % 2)+1)%2
+e = ((e % 2)+1)%2
+print(b+c+d+e)
