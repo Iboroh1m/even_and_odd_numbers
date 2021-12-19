367b4edb31ae6fd23365e29e67df2a154549c012
Iboroh1m <boymirzayev20031310@gmail.com>
2021-12-19 00:20:17 -0800

    sum of odd numbers

--- number_odd.py
+++ number_odd.py
@@ -1,5 +1,12 @@
-#A four-digit integer is given. Find the number of odd digits in it.
-
-#Create a variable "var_int" and assign it a four-digit integer value.
-
-#Print the number of odd digits in the variable "var_int".
+var_int = 7857
+a = var_int // 10             
+b = var_int % 10              
+c = a %10                     
+a = a //10                     
+d = a % 10                   
+e = a // 10
+b = (b % 2) %2
+c = (c % 2) %2
+d = (d % 2) %2
+e = (e % 2) %2
+print(b+c+d+e)
